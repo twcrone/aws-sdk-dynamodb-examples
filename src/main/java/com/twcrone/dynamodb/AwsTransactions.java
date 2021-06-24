3e484d2ca588e0fb4882794392b5a8eda0a171f0
Todd W Crone <twcrone@gmail.com>
2021-06-24 12:05:13 -0400

    Spread out transactions

--- src/main/java/com/twcrone/dynamodb/AwsTransactions.java
+++ src/main/java/com/twcrone/dynamodb/AwsTransactions.java
@@ -4,6 +4,7 @@ import com.newrelic.api.agent.Trace;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AwsTransactions {
     private final CustomerRepository repository;
@@ -16,6 +17,7 @@ public class AwsTransactions {
         try {
             //System.out.format("Retrieving item \"%s\" from \"%s\"\n", keyVal, tableName );
             System.out.println("Processing java devs...");
+/*
             List<String> ids = Flux.just(
                     new Customer("Jason", "jason@nr.com", "Portland"),
                     new Customer("Xi Xia", "xixia@nr.com", "Portland"),
@@ -30,12 +32,33 @@ public class AwsTransactions {
                     .flatMap(repository::updateCustomer)
                     .flatMap(repository::deleteCustomer)
                     .collectList().block();
+*/
+            List<Customer> devs = Flux.just(
+                    new Customer("Jason", "jason@nr.com", "Portland"),
+                    new Customer("Xi Xia", "xixia@nr.com", "Portland"),
+                    new Customer("Brad", "brad@nr.com", "Portland"),
+                    new Customer("André", "andre@nr.com", "Virginia"),
+                    new Customer("Kevyn", "kevyn@nr.com", "Jersey"),
+                    new Customer("Chinmay", "chinmay@nr.com", "Berkley"),
+                    new Customer("Todd", "todd@nr.com", "Triangle"))
+                    .flatMap(repository::createCustomer)
+                    .collectList()
+                    .block();
+
+            Thread.sleep(3000);
 
             repository.listCustomers()
                     .doOnEach(System.out::println)
                     .collectList().block();
 
-            System.out.println(ids);
+            Thread.sleep(3000);
+
+            Flux.fromIterable(devs)
+                    .map(Customer::getId)
+                    .flatMap(repository::deleteCustomer)
+                    .collectList()
+                    .block();
+            //System.out.println(ids);
         } catch (Exception e) {
             e.printStackTrace();
         }
